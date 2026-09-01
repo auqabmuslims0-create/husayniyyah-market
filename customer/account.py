5cb076cb45070634146da6d674bdbe7056d7c529
Auqab <your-email@example.com>
2026-09-01 12:48:23 +0300

    إضافة استيراد jsonify في customer/account.py

--- customer/account.py
+++ customer/account.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash, abort
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash, abort, jsonify
 from sqlalchemy.orm import joinedload
 from database import db
 import models
